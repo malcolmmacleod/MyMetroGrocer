bee9a4911feae0e5ec33ccd579a51925586c25c5
malcolmmacleod <malcolm@macleodhome.com>
2012-05-15 15:17:50 -0700

    Another set of initial files 2

---
+++ MyMetroGrocer/js/default.js
@@ -0,0 +1,51 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+
+    app.onactivated = function (eventObject) {
+        if (eventObject.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
+            if (eventObject.detail.previousExecutionState !== Windows.ApplicationModel.Activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize 
+                // your application here.
+                performInitialSetup(eventObject);
+            } else {
+                // TODO: This application has been reactivated from suspension. 
+                // Restore application state here.
+                performRestore(eventObject);
+            }
+            WinJS.UI.processAll();
+        }
+    };
+
+    app.oncheckpoint = function (eventObject) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the 
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // eventObject.setPromise().
+        performSuspend(eventObject);
+
+    };
+
+    app.start();
+
+    function performInitialSetup(e) {
+        WinJS.Binding.processAll(document.body, ViewModel);
+
+        WinJS.Utilities.query('#newZipButton').listen("click", function (e) {
+            ViewModel.UserData.homeZipCode = WinJS.Utilities.query('#newZip')[0].value;
+        });
+    }
+
+    function performRestore(e) {
+        // TODO
+    }
+
+    function performSuspend(e) {
+        // TODO
+    }
+})();
---
+++ MyMetroGrocer/js/viewmodel.js
@@ -0,0 +1,51 @@
+﻿/// <reference path="//Microsoft.WinJS.0.6/js/base.js" />
+/// <reference path="//Microsoft.WinJS.0.6/js/ui.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("ViewModel", 
+        {UserData: WinJS.Binding.as({
+            // private members
+            _shoppingItems: [],
+            _preferredStores: [],
+
+            // public members
+            homeZipCode: null,
+
+            getStores: function () {
+                return this._preferredStores;
+            },
+
+            addStore: function (newStore) {
+                this._preferredStores.push(newStore);
+            },
+
+            getItems: function () {
+                return this._shoppingItems;
+            },
+
+            addItem: function (newName, newQuantity, newStore) {
+                this._shoppingItems.push({
+                    item: newName,
+                    quantity: newQuantity,
+                    store: newStore
+                });
+            }
+        })
+    });
+
+
+    ViewModel.UserData.homeZipCode = "NY 10118";
+
+    ViewModel.UserData.addStore("Whole Foods");
+    ViewModel.UserData.addStore("Kroger");
+    ViewModel.UserData.addStore("Costco");
+    ViewModel.UserData.addStore("Walmart");
+
+    ViewModel.UserData.addItem("Apples", 4, "Whole Foods");
+    ViewModel.UserData.addItem("Hot dogs", 12, "Costco");
+    ViewModel.UserData.addItem("Soda", "4 pack", "Costco");
+
+
+})();
